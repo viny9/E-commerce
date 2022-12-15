f1df3efa3d514c2db6e97cc27bbade34546cc314
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2022-12-15 19:45:39 -0300

    Main UI completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MainComponent } from './components/main/main.component';
+import { HeadComponent } from './components/head/head.component'
 
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { MainComponent } from './components/main/main.component';
-import { HeadComponent } from './components/head/head.component'
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -33,6 +34,7 @@ import { HeadComponent } from './components/head/head.component'
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
 
   ],
   providers: [],
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  imgs = [
+    {url: 'https://images.tcdn.com.br/static_inst/meiosdepagamento/wp-content/uploads/2021/07/opencommerces.jpg '},
+    {url: 'https://images.tcdn.com.br/static_inst/meiosdepagamento/wp-content/uploads/2021/07/opencommerces.jpg '},
+    
+  ]
+
   constructor() { }
   
   ngOnInit(): void {
+  }
+
+  toRight() {
+    const teste = document.querySelector('.teste2')
+    
+    teste?.classList.toggle('teste3')
+    teste?.classList.remove('teste')
+  }
+  
+  toLeft() {
+    const teste = document.querySelector('.teste2')
+
+    teste?.classList.toggle('teste')
+    teste?.classList.remove('teste3')
   }
 }
