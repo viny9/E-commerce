af9c165381b2414ff41c4e1c2cc17c220f3075e5
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2022-12-26 19:07:04 -0300

    Componente Cart

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  amount:number = 1
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  subAmount() {
+    if (this.amount > 1) {
+      this.amount -= 1
+    }
+  }
+
+  addAmount() {
+    const max = 20
+
+    if (this.amount < max) {
+      this.amount += 1
+    } 
+  }
+}
