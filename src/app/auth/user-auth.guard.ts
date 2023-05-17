92d92e05756efe492f3dda409a1f74640750e37c
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-05-17 12:24:21 -0300

    Correção no auth guard

--- src/app/auth/user-auth.guard.ts
+++ src/app/auth/user-auth.guard.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, CanActivateChild, UrlTree } from '@angular/router';
 import { ProductService } from '../services/product/product.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserAuthGuard implements CanActivate {
+export class UserAuthGuard implements CanActivate, CanActivateChild {
 
   constructor(private db: ProductService, private auth: AngularFireAuth) { }
+
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const token = localStorage['token']
@@ -16,10 +17,36 @@ export class UserAuthGuard implements CanActivate {
 
     if (token && admin === false) {
       return true
+    } else if (token && admin === true) {
+      this.db.navegate('/admin/products')
+      return false
+    } else if (token === undefined) {
+      return true
     } else {
-      this.db.navegate('/admin')
+      this.db.navegate('')
       return false
     }
   }
 
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = localStorage['token']
+    const routeName = childRoute.routeConfig?.path
+
+    enum routes {
+      userComponent = 'user',
+      orders = 'allOrders',
+      order = 'order/:orderId'
+    }
+
+    const blockedRoutes = routeName === routes.userComponent || routeName === routes.orders || routeName === routes.order
+
+    if (blockedRoutes && token === undefined) {
+      this.db.navegate('')
+      return false
+    } else {
+      return true
+    }
+
+
+  }
 }
