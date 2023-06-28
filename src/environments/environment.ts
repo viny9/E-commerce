bb98ce2736ee467aa4f773cccd146c4ba9788083
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-06-28 00:58:30 -0300

    Teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
-(window as any).process = {}
-
 import { firebase } from './../../env';
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
   production: false,
