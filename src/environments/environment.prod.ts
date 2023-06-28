14299c4defdb155b34d16ea5e2f7cbbc6d466ebf
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-06-28 12:13:39 -0300

    Correção completa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,7 @@
-(window as any).process = {}
-console.log(process.env);
-
+import { firebase } from '../../env'
 
 export const environment = {
   production: true,
   backendBaseUrl: 'https://ecommerce-back-0gyg.onrender.com',
-  firebase: {
-    apiKey: process.env['apiKey'],
-    authDomain: process.env['authDomain'],
-    projectId: process.env['projectId'],
-    storageBucket: process.env['storageBucket'],
-    messagingSenderId: process.env['messagingSenderId'],
-    appId: process.env['appId'],
-    measurementId: process.env['measurementId']
-  }
+  firebase: firebase
 };
