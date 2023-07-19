baa88979da75b80782656a7ed5744968feed0793
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2023-07-18 22:49:37 -0300

    Correção de erros

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,6 +8,6 @@ export const environment = {
     storageBucket: process.env['STORAGE_BUCKET'],
     messagingSenderId: process.env['MESSAGING_SENDER_ID'],
     appId: process.env['APP_ID'],
-    measurementId: process.env['MEASuREMENT_ID']
+    measurementId: process.env['MEASUREMENT_ID']
   }
 };
