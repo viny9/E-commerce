d68902f46cb9874818fe9fbcf4adb5b89adce5e2
Vinicius Carvalho <vinicius.carvalho@dbserver.com.br>
2024-05-30 11:51:25 -0300

    Migrate user service to api

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,225 +1,95 @@
 import { User } from './../../models/user';
-import { NoopScrollStrategy } from '@angular/cdk/overlay';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { DialogUpdateUserComponent } from 'src/app/views/dialog-update-user/dialog-update-user.component';
 import { environment } from 'src/environments/environment';
-import { StripeService } from '../stripe/stripe.service';
-import { catchError, lastValueFrom, map } from 'rxjs';
+import { catchError, filter } from 'rxjs';
 import { ErrorsService } from '../errors/errors.service';
-import { ProductService } from '../product/product.service';
+import { userMessages } from 'src/app/utils/snackbar';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private userId: string = localStorage['userId'];
+  private baseUrl: string = environment.backendBaseUrl;
 
-  userId = localStorage['userId']
-  baseUrl: string = environment.backendBaseUrl
+  constructor(
+    private snackbar: MatSnackBar,
+    private http: HttpClient,
+    private errorService: ErrorsService,
+    private router: Router
+  ) {}
 
-  constructor(private snackBar: MatSnackBar, private router: Router, private http: HttpClient, private dialog: MatDialog, private productService: ProductService, private stripeService: StripeService, private errorService: ErrorsService) { }
-
-  // Login e cadastro
-
-
-  async newAdminUser(user: any) {
-    try {
-      await this.auth.createUserWithEmailAndPassword(user.email, user.password)
-      delete user.password
-      user.admin = true
-
-      await this.createUser(user)
-      await Promise.all([this.userMessages('Usuário criado'), this.navegate('admin/adminUsersList')])
-
-    } catch (error) {
-      this.errorService.handleError(error)
-    }
+  async newAdminUser(user: User) {
+    return this.http
+      .post(`${this.baseUrl}/user`, user)
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)))
+      .subscribe(() => {
+        userMessages('Usuário criado', this.snackbar);
+        this.router.navigate(['admin/adminUsersList']);
+      });
   }
 
-  async userInfos(item: User) {
-    const query = this.firestore.collection('users', ref => ref.where('email', '==', item.email));
-    const doc = await lastValueFrom<any>(query.get().pipe(
-      map(res => {
-        const doc = res.docs[0];
-        return doc ? doc : null;
-      })
-    ))
-
-    const user = doc.data()
-
-    const userInfos = {
-      userId: doc.id,
-      admin: user.admin
-    }
-
-    return userInfos
-  }
-
-  // Informações do usuário
   getUsers() {
-    return this.firestore.collection('users').get()
-      .pipe(
-        map((res) => {
-          return res.docs.map((doc) => doc.data())
-        }),
-        catchError((e: Error) => this.errorService.handleError(e))
-      )
+    return this.http
+      .get(`${this.baseUrl}/user`)
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)));
   }
 
   getAdminUsers() {
-    const query = this.firestore.collection('users', ref => ref.where('admin', '==', true))
-    return query.get().pipe(
-      catchError((e: Error) => this.errorService.handleError(e))
-    )
-
+    return this.http
+      .get<User>(`${this.baseUrl}/user`)
+      .pipe(filter((user) => user.isAdmin))
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)));
   }
 
-  getUserById(id: any) {
-    return this.firestore.collection('users').doc(id).get()
-      .pipe(
-        catchError((e: Error) => this.errorService.handleError(e))
-      )
+  getUserById(id: string) {
+    return this.http
+      .get<User>(`${this.baseUrl}/user/${id}`)
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)));
   }
 
   createUser(userInfos: User) {
-    return this.firestore.collection('users').add(userInfos)
-      .catch((e: Error) => this.errorService.handleError(e))
+    return this.http
+      .post(`${this.baseUrl}/user`, userInfos)
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)));
   }
 
-  async updateUser(updatedInfos: User) {
-    const auth: any = getAuth().currentUser
-
-    if (updatedInfos.email != updatedInfos.oldEmail) {
-      try {
-
-        await updateEmail(auth, updatedInfos.email) // Atualiza o authenticator
-          .catch(() => {  // Abri um dialog para fazer login caso necessário
-            this.dialog.open(DialogUpdateUserComponent, {
-              data: updatedInfos,
-              width: '500px',
-              scrollStrategy: new NoopScrollStrategy()
-            })
-          })
-        delete updatedInfos.oldEmail
-
-        await this.firestore.collection('users').doc(this.userId).update(updatedInfos) // Atualiza o firestore
-        this.stripeService.updateCustomer(updatedInfos).subscribe() //Atualiza o stripe
-
-        this.userMessages('Usuário atualizado')
-
-      } catch (error) {
-        this.errorService.handleError(error)
-      }
-
-    } else {
-      try {
-        delete updatedInfos.oldEmail
-
-        await this.firestore.collection('users').doc(this.userId).update(updatedInfos) // Atualiza o firestore
-        this.stripeService.updateCustomer(updatedInfos).subscribe() //Atualiza o stripe
-        this.userMessages('Usuário atualizado')
-
-      } catch (error) {
-        this.errorService.handleError(error)
-      }
-    }
+  async updateUser(id: string, updatedInfos: User) {
+    return this.http
+      .put(`${this.baseUrl}/user/${id}`, updatedInfos)
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)))
+      .subscribe(() => userMessages('Usuário atualizado', this.snackbar));
   }
 
-  async updateUserWithLogin(user: any) {
-    const auth: any = getAuth()
-
-    try {
-      this.auth.signInWithEmailAndPassword(user.oldEmail, user.password)
-
-      await updateEmail(auth, user.email) // Atualiza o authenticator
-      delete user.oldEmail && delete user.password
-
-      await this.firestore.collection('users').doc(this.userId).update(user)  // Atualiza o firestore
-      this.stripeService.updateCustomer(user).subscribe()  //Atualiza o stripe
-      this.userMessages('Dados atualizados')
-
-    } catch (error) {
-      this.errorService.handleError(error)
-    }
+  async deleteUser(user: User) {
+    return this.http
+      .delete(`${this.baseUrl}/user/${user.id}`)
+      .pipe(catchError((e: Error) => this.errorService.handleError(e)));
   }
 
-  async deleteUser(user: any) {
-    try {
-      await this.auth.signInWithEmailAndPassword(user.email, user.password)
-      await getAuth().currentUser?.delete() // Exclui o usuário no authentic
-      await this.firestore.collection('users').doc(this.userId).delete() // Exclui o usuário da firestore
-      this.stripeService.deleteCustomer(user.stripe_id).subscribe() //Exclui o  usuário no stripe
-    } catch (error) {
-      this.errorService.handleError(error)
-    }
-  }
+  // // Emails
+  // sendVerificationCodeEmail(email: string) {
+  //   const body = {
+  //     email: email,
+  //   };
 
-  async updateUserAsAdmin(user: any) {
-    const query = this.firestore.collection('users', ref => ref.where('email', '==', user.oldEmail));
-    const id = await lastValueFrom<any>(query.get().pipe(
-      map(res => {
-        const doc = res.docs[0];
-        return doc ? doc.id : null;
-      })
-    ))
+  //   return this.http
+  //     .post(`${this.baseUrl}/recoverPassword`, body)
+  //     .pipe(catchError((e: Error) => this.errorService.handleError(e)));
+  // }
 
-    const newUser = {
-      ...user
-    }
-
-    delete newUser.password && user.password
-    delete newUser.confirmPassword && user.confirmPassword
-    delete newUser.oldEmail
-
-    await this.firestore.collection('users').doc(id).update(newUser)
-    return this.http.put(`${this.baseUrl}/updateUser`, user)
-      .pipe(
-        catchError((e: Error) => this.errorService.handleError(e))
-      )
-  }
-
-  async deleteUserAsAdmin(user: any) {
-    const id = await this.productService.getId('users', user)
-
-    await this.firestore.collection('users').doc(id).delete()
-    return this.http.post(`${this.baseUrl}/deleteUser`, user)
-      .pipe(
-        catchError((e: Error) => this.errorService.handleError(e))
-      )
-  }
-
-
-  // Emails
-  sendVerificationCodeEmail(email: string) {
-    const body = {
-      email: email
-    }
-
-    return this.http.post(`${this.baseUrl}/recoverPassword`, body)
-      .pipe(
-        catchError((e: Error) => this.errorService.handleError(e))
-      )
-  }
-
-  async sendPasswordResetEmail(email: string) {
-    try {
-      await this.auth.sendPasswordResetEmail(email)
-      await Promise.all([this.userMessages('Foi enviado um email para atualizar senha'), this.navegate('/signIn')])
-    } catch (error) {
-      this.errorService.handleError(error)
-    }
-  }
-
-  // // Extras
-  // userMessages(message: string) {
-  //   this.snackBar.open(message, 'X', {
-  //     duration: 2000,
-  //     horizontalPosition: 'end',
-  //     verticalPosition: 'top',
-  //     panelClass: 'snackBar'
-  //   })
+  // async sendPasswordResetEmail(email: string) {
+  //   try {
+  //     await this.auth.sendPasswordResetEmail(email);
+  //     await Promise.all([
+  //       this.userMessages('Foi enviado um email para atualizar senha'),
+  //       this.navegate('/signIn'),
+  //     ]);
+  //   } catch (error) {
+  //     this.errorService.handleError(error);
+  //   }
   // }
 }
