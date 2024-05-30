db222c177b88e0d1dcb33c4882f611f290b8aa31
Vinicius Carvalho <vinicius.carvalho@dbserver.com.br>
2024-05-30 14:54:08 -0300

    Typing and adjusts the services call

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,87 +1,86 @@
 import { Component, OnInit } from '@angular/core';
-import { AdminRoutes } from 'src/app/enums/admin-routes';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { ProductService } from 'src/app/services/product/product.service';
-import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  public isSidebarOpen: boolean = false;
+  public sidebarMode: any = '';
+  public checked: boolean = false;
+  public selected: string = '';
+  private theme: string = localStorage['theme'];
 
-  selected: string = ''
-  theme: string | null = localStorage['theme']
-  checked: boolean = false
-  routes = AdminRoutes
-  sidebarMode: any = ''
-  isSidebarOpen: boolean = false
-
-  constructor(private userService: UserService, private db: ProductService) {
-    db.updatedComponent.subscribe((res: any) => {
-      this.selected = res
-    })
+  constructor(
+    private db: ProductService,
+    private authService: AuthService
+  ) {
+    this.db.updatedComponent.subscribe((res: any) => {
+      this.selected = res;
+    });
   }
 
   ngOnInit(): void {
-    this.themeIcon()
+    this.themeIcon();
 
     if (window.screen.width <= 768) {
-      this.sidebarMode = 'over'
+      this.sidebarMode = 'over';
     } else {
-      this.sidebarMode = 'side'
-      this.isSidebarOpen = true
+      this.sidebarMode = 'side';
+      this.isSidebarOpen = true;
     }
   }
 
   themeIcon() {
-    const sun = document.querySelector('.sun')
-    const moon = document.querySelector('.moon')
+    const sun = document.querySelector('.sun');
+    const moon = document.querySelector('.moon');
 
-    if (this.theme === "dark") {
-      sun?.classList.toggle('hide')
-      this.checked = true
+    if (this.theme === 'dark') {
+      sun?.classList.toggle('hide');
+      this.checked = true;
     } else {
-      moon?.classList.toggle('hide')
-      this.checked = false
+      moon?.classList.toggle('hide');
+      this.checked = false;
     }
   }
 
   themeToggle() {
-    const sun = document.querySelector('.sun')
-    const moon = document.querySelector('.moon')
+    const sun = document.querySelector('.sun');
+    const moon = document.querySelector('.moon');
 
-    if (this.theme == "dark") {
-      this.theme = 'light'
-      sun?.classList.remove('hideAnimation')
-      moon?.classList.remove('showAnimation')
+    if (this.theme == 'dark') {
+      this.theme = 'light';
+      sun?.classList.remove('hideAnimation');
+      moon?.classList.remove('showAnimation');
 
-      sun?.classList.toggle('showAnimation')
-      moon?.classList.toggle('hideAnimation')
+      sun?.classList.toggle('showAnimation');
+      moon?.classList.toggle('hideAnimation');
 
-      localStorage.setItem('theme', 'light')
-      document.body.classList.toggle('lightMode')
-
+      localStorage.setItem('theme', 'light');
+      document.body.classList.toggle('lightMode');
     } else {
-      this.theme = 'dark'
-      sun?.classList.remove('showAnimation')
-      moon?.classList.remove('hideAnimation')
+      this.theme = 'dark';
+      sun?.classList.remove('showAnimation');
+      moon?.classList.remove('hideAnimation');
 
-      sun?.classList.toggle('hideAnimation')
-      moon?.classList.toggle('showAnimation')
+      sun?.classList.toggle('hideAnimation');
+      moon?.classList.toggle('showAnimation');
 
-      document.body.classList.toggle('lightMode')
-      localStorage.setItem('theme', 'dark')
+      document.body.classList.toggle('lightMode');
+      localStorage.setItem('theme', 'dark');
     }
   }
 
   closeSidebar(drawer: any) {
     if (window.screen.width <= 768) {
-      drawer.toggle()
+      drawer.toggle();
     }
   }
 
   signOut() {
-    this.userService.logOut()
+    this.authService.logOut();
   }
 }
