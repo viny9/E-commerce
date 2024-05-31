8cc7cf454740a976994bd0909415080e3f9514f5
Vinicius Carvalho <vinicius.carvalho@dbserver.com.br>
2024-05-30 22:28:21 -0300

    fix search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,64 +1,71 @@
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoadService } from 'src/app/services/load/load.service';
 import { ProductService } from 'src/app/services/product/product.service';
+import { userMessages } from 'src/app/utils/snackbar';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  public products: any;
+  public searchWord: any;
+  public loading: any = false;
+  public selectedFilters: any = [];
+  public screenSize: number = window.screen.width;
+  private filtersOptions: any = [];
+  private selected: any = -1;
+  private filterProducts: any = [];
 
-  products: any
-  searchWord: any
-  loading: any = false
-  filtersOptions: any = []
-  selectedFilters: any = []
-  selected: any = -1
-  filterProducts: any = []
-  screenSize:number = window.screen.width
+  dataSource = new MatTreeNestedDataSource();
+  treeControl = new NestedTreeControl((node: any) => node.children);
 
-  dataSource = new MatTreeNestedDataSource()
-  treeControl = new NestedTreeControl((node: any) => node.children)
-
-  constructor(private db: ProductService, private loadService: LoadService, private router: ActivatedRoute) {
+  constructor(
+    private db: ProductService,
+    private loadService: LoadService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
+  ) {
     loadService.isLoading.subscribe((res) => {
-      this.loading = res
-    })
+      this.loading = res;
+    });
 
-    this.filters()
+    this.filters();
   }
 
-  hasChild = (_: number, node: any) => !!node.children && node.children.length > 0;
+  hasChild = (_: number, node: any) =>
+    !!node.children && node.children.length > 0;
 
   // Adicionar mais opções de filtro
   // Melhorar este código
   // Caso não tenha produtos por fato de filtro adicionar uma mensagem
 
   ngOnInit(): void {
-    this.getProducts()
+    this.getProducts();
   }
 
   getProducts() {
-    this.loadService.showLoading()
+    this.loadService.showLoading();
 
-    this.router.params.subscribe((word: any) => {
-      const searchWord = word.searchWord
-      this.searchWord = searchWord
+    this.route.params.subscribe((word: any) => {
+      const searchWord = word.searchWord;
+      this.searchWord = searchWord;
 
       this.db.getProducts().subscribe((res: any) => {
-
         const search = res.filter((product: any) => {
-          return product.name.toLowerCase().includes(searchWord.toLowerCase())
-        })
+          return product.name.toLowerCase().includes(searchWord.toLowerCase());
+        });
 
-        this.products = search
-        this.loadService.hideLoading()
-      })
-    })
+        this.products = search;
+        this.loadService.hideLoading();
+      });
+    });
   }
 
   filters() {
@@ -71,7 +78,7 @@ export class SearchComponent implements OnInit {
             type: 'price',
             checked: false,
             min: 0,
-            max: 100
+            max: 100,
           },
 
           {
@@ -107,126 +114,124 @@ export class SearchComponent implements OnInit {
             name: 'teste',
             type: 'category',
             checked: false,
-            value: 'teste'
+            value: 'teste',
           },
           {
             name: 'Eletronicos',
             type: 'category',
             checked: false,
-            value: 'Eletronicos'
+            value: 'Eletronicos',
           },
           {
             name: 'Mouses',
             type: 'category',
             checked: false,
-            value: 'Mouses'
+            value: 'Mouses',
           },
-        ]
-      }
-    ]
+        ],
+      },
+    ];
 
-    this.dataSource.data = this.filtersOptions
+    this.dataSource.data = this.filtersOptions;
   }
 
-  async selectProduct(product: any) {
-    const id: any = await this.db.getId(this.db.path.products, product)
-    this.db.navegate(`product/${id}`)
+  async selectProduct(id: string) {
+    this.router.navigate([`product/${id}`]);
   }
 
   selectedFilter(selectedFilter: any) {
-    this.loadService.showLoading()
+    this.loadService.showLoading();
 
-    this.selectedFilters.push(selectedFilter)
+    this.selectedFilters.push(selectedFilter);
 
     const duplicateFilter = this.selectedFilters.filter((filter: any) => {
-      return filter.name === selectedFilter.name
-    })
+      return filter.name === selectedFilter.name;
+    });
 
     const twoPriceFilter = this.selectedFilters.filter((filter: any) => {
-      return filter.type === 'price'
-    })
+      return filter.type === 'price';
+    });
 
     if (duplicateFilter.length <= 1 && twoPriceFilter.length <= 1) {
-      this.addFilter()
+      this.addFilter();
 
       if (this.products.length === 0) {
-        this.getProducts()
+        this.getProducts();
       }
-
     } else if (twoPriceFilter.length > 1) {
       this.selectedFilters.pop();
-      this.db.userMessages('Só pode usar um filtro de preço por vez.')
-
+      userMessages('Só pode usar um filtro de preço por vez.', this.snackBar);
     } else if (duplicateFilter.length > 1) {
       this.selectedFilters.pop();
-      this.db.userMessages('Esse filtro já está em uso.')
+      userMessages('Esse filtro já está em uso.', this.snackBar);
     }
   }
 
   addFilter() {
-    this.router.params.subscribe((word: any) => {
-      const searchWord = word.searchWord
+    this.route.params.subscribe((word: any) => {
+      const searchWord = word.searchWord;
 
       this.db.getProducts().subscribe((res: any) => {
-
         let search = res.filter((product: any) => {
-          return product.name.toLowerCase().includes(searchWord.toLowerCase())
-        })
+          return product.name.toLowerCase().includes(searchWord.toLowerCase());
+        });
 
-        const allProducts = search
+        const allProducts = search;
 
         for (let filter of this.selectedFilters) {
-
           if (filter.type === 'category') {
             // Remove os items duplicados do array
-            this.filterProducts = this.filterProducts.filter((value: any, index: any, self: any) =>
-              index === self.findIndex((t: any) => (
-                t.place === value.place && t.name === value.name
-              ))
-            )
+            this.filterProducts = this.filterProducts.filter(
+              (value: any, index: any, self: any) =>
+                index ===
+                self.findIndex(
+                  (t: any) => t.place === value.place && t.name === value.name
+                )
+            );
 
             search = allProducts.filter((product: any) => {
-              return product.category === filter.name
-            })
-
-            search.forEach((element: any) => {
-              this.filterProducts.push(element)
+              return product.category === filter.name;
             });
 
-            search = this.filterProducts
+            search.forEach((element: any) => {
+              this.filterProducts.push(element);
+            });
 
+            search = this.filterProducts;
           } else if (filter.type === 'price') {
-
-            search = search.filter((value: any, index: any, self: any) =>
-              index === self.findIndex((t: any) => (
-                t.place === value.place && t.name === value.name
-              ))
-            )
+            search = search.filter(
+              (value: any, index: any, self: any) =>
+                index ===
+                self.findIndex(
+                  (t: any) => t.place === value.place && t.name === value.name
+                )
+            );
 
             search = search.filter((product: any) => {
-              return product.price >= filter.min && product.price <= filter.max
-            })
+              return product.price >= filter.min && product.price <= filter.max;
+            });
           }
         }
 
-        this.products = search
+        this.products = search;
 
-        this.loadService.hideLoading()
-      })
-    })
+        this.loadService.hideLoading();
+      });
+    });
   }
 
   removeFilter(filter: any) {
-    const index = this.selectedFilters.findIndex((i: any) => i.name === filter.name);
+    const index = this.selectedFilters.findIndex(
+      (i: any) => i.name === filter.name
+    );
     this.selectedFilters.splice(index, 1);
 
     if (this.selectedFilters.length === 0) {
-      this.getProducts()
+      this.getProducts();
     } else if (this.selectedFilters.length > 0) {
-      this.loadService.showLoading()
-      this.filterProducts = []
+      this.loadService.showLoading();
+      this.filterProducts = [];
       this.addFilter();
     }
   }
-
 }
